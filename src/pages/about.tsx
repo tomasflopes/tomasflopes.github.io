3c688f4c8989f24d2690650a7ca590ba3fc4eb07
Tomás Lopes <tomas050302@gmail.com>
2022-07-24 23:55:37 +0100

    feat: added typewriting animation to about me page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,28 @@
 import type { NextPage } from "next";
+
+import {
+  Container,
+  Flex,
+  Heading,
+  keyframes,
+} from "@chakra-ui/react";
 import Head from "next/head";
-
-import { Container, Heading } from "@chakra-ui/react";
-
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
+const typewriter = keyframes`  
+    from{width: 0;}
+    to{width: 5em;}
+    `;
+
+const blinkTextCursor = keyframes`  
+  from{border-right-color: rgba(255,255,255,.75);}
+to{border-right-color: transparent;}
+  `;
+
 const about: NextPage = () => {
+  const typewriterAnimation = `${typewriter} .8s steps(44) 1s 1 normal both, ${blinkTextCursor} .8s steps(22) 5 normal forwards`;
+
   return (
     <>
       <Head>
@@ -15,9 +31,22 @@ const about: NextPage = () => {
       </Head>
       <Container minH="93vh" minW={"100%"}>
         <Navbar />
-        <Heading as="h1" fontSize={"4xl"} borderBottom="1px" my="2">
-          About me
-        </Heading>
+        <Flex>
+          <Heading
+            as="h1"
+            fontSize={"4xl"}
+            my="2"
+            animation={{ md: typewriterAnimation }}
+            overflow="hidden"
+            borderRight={{ md: "2px" }}
+            whiteSpace="nowrap"
+            w="100%"
+            textAlign={{ base: "center", md: "left" }}
+            borderColor="rgba(255,255,255,.75)"
+          >
+            About me
+          </Heading>
+        </Flex>
       </Container>
       <Footer />
     </>
